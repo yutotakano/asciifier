df49e2e9c44433c803b0133ae7d9f78904ac2030
yutotakano <moa17stock@gmail.com>
2020-05-02 12:45:42 +0900

    Add more comments and argument options

--- asciifier.py
+++ asciifier.py
@@ -9,36 +9,38 @@ import unicodedata
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate an ASCII art file using text input or image input.')
     input_group = parser.add_mutually_exclusive_group(required=True)
-    input_group.add_argument('-i', '--image', help='Any type of Image that cv2 supports')
-    input_group.add_argument('-t', '--text', help='Some text')
-    parser.add_argument('-f', '--font', help='Font for the text to be used for display. Calculation always uses FiraCode-Medium')
+    input_group.add_argument('-i', '--image', help='Any type of image file that cv2 supports')
+    input_group.add_argument('-t', '--text', help='Some string of text')
     parser.add_argument('-c', '--charset', help='Character set text file, UTF-8, Ignores full-width chars')
-    parser.add_argument('height', help='Output height in number of lines', nargs='?', default=None)
-    parser.add_argument('width', help='Output width in number of characters', nargs='?', default=None)
     parser.add_argument('-o', '--output', help='Output file name', required=True)
+    parser.add_argument('--height', help='Output height in number of lines, also serves as fontsize')
+    parser.add_argument('--width', help='Output width in number of characters')
+    parser.add_argument('--font', help='Font for the text to be used for display. Matrix pattern calculation always uses FiraCode-Medium')
+    parser.add_argument('--ratio', help='How many times taller each character is compared to its width', default='2.3')
+    parser.add_argument('--invert', action='store_true', help='Invert image when set')
+    parser.add_argument('--matrix', help='Matrix size for sampling', default='5')
     
     args = parser.parse_args()
 
-    matrix_size = 5
-
-    horizontal_vertical_ratio = 2.3
+    matrix_size = int(args.matrix)
+    horizontal_vertical_ratio = float(args.ratio)
 
     if args.image is not None:
         # Image is set
         
         # 0 = grayscale
         im = cv2.imread(args.image, 0)
-        im = cv2.bitwise_not(im)
         
         if im is None:
             print('Image not found.')
             exit()
-    else:        
+
+    else:
         if args.font is None:
-            parser.error('--font argument is required')
+            parser.error('Missing argument --font')
             exit()
         if args.height is None:
-            parser.error('Height is required for font')
+            parser.error('Missing argument <height> for font')
 
         im = Image.new('L', (len(args.text)*int(args.height)*matrix_size, int(args.height)*matrix_size), 255)
         font = ImageFont.truetype(args.font, int(args.height)*matrix_size - 2)
@@ -46,8 +48,11 @@ if __name__ == "__main__":
         context.text((2, 0), args.text, 0, font)
 
         im = np.array(im)
+
+    if args.invert is True:
+        im = cv2.bitwise_not(im)
     
-    # Manipulating im to a array of sampled values
+    # Alright, time to move onto sampling `im`
 
     source_height, source_width = im.shape[:2]
         
@@ -61,10 +66,10 @@ if __name__ == "__main__":
         rectsize_w = round(rectsize_h / horizontal_vertical_ratio)
     elif args.width is not None:
         rectsize_w = math.ceil(source_width / (int(args.width) * horizontal_vertical_ratio))
-        rectsize_h = rectsize_w * horizontal_vertical_ratio
+        rectsize_h = round(rectsize_w * horizontal_vertical_ratio)
     else:
         rectsize_h = 10
-        rectsize_w = 10
+        rectsize_w = 5
     
     output_height = math.ceil(source_height / rectsize_h)
     output_width = math.ceil(source_width / rectsize_w)
@@ -87,7 +92,7 @@ if __name__ == "__main__":
 
             Z[y_index][x_index] = resized_padded_crop_region
         
-    # Great I don't even remember what I was doing above but it kinda seems to work
+    # Great I don't even remember what I was doing above when I wrote it yesterday night at 4am but it kinda seems to work
     # Now we have a 4 dimensional array
     # [ for each height
     #   [ for each width
@@ -99,8 +104,10 @@ if __name__ == "__main__":
     #   ]
     # ]
 
+    # Not necessary but having values between 0-1 makes me feel better
     Z = Z / 255.0
 
+    # Default charset ASCII if none is specified
     charset = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]\\^_`abcdefghijklmnopqrstuvwxyz{|}~'
     if args.charset is not None:
         try:
@@ -109,24 +116,27 @@ if __name__ == "__main__":
                 charset = unicodedata.normalize('NFKC', charset)
         except Exception as e:
             pass
+    
+    # For each character, create a matrix and append it to `data`
  
+    char_data = []
+
     # Use FiraCode because it's a fairly standard Console font and ascii art will be displayed in a similar font most of the time
     font = ImageFont.truetype(r"FiraCode-Medium.ttf", matrix_size)
-    letter_image = Image.new('L', (matrix_size, matrix_size), 255)
- 
-    data = []
+    letter_image = Image.new('L', (round(matrix_size / horizontal_vertical_ratio), matrix_size), 255)
     for char in charset:
         char_width = unicodedata.east_asian_width(char)
-        if char_width in ['F', 'W', 'A']:
+        # Don't check for 'A' (Ambiguous) because that would eliminate █ as well
+        if char_width in ['F', 'W']:
             continue
         canvas = ImageDraw.Draw(letter_image)
         canvas.text((0, 0), char, 0, font)
-        data.append({
-            'actual_character': char,
-            'data': np.array(letter_image) / 255.0
+        char_data.append({
+            'character': char,
+            'matrix': cv2.resize(np.array(letter_image), (matrix_size, matrix_size)) / 255.0
         })
         # blank canvas again
-        canvas.rectangle((0, 0, matrix_size, matrix_size), 255)
+        canvas.rectangle((0, 0, round(matrix_size / horizontal_vertical_ratio), matrix_size), 255)
     
     
     final_output = ''
@@ -137,8 +147,8 @@ if __name__ == "__main__":
             # 1. for each character calculate: the sum of (the element-wise absolute difference between rectangle data and character data)
             # 2. find the indices of the one with the minimum total
             # 3. output the textual representation of that
-            indice = np.argmin([np.sum(np.absolute(rectangle - char['data'])) for char in data])
-            final_output = f"{final_output}{data[indice]['actual_character']}"
+            indice = np.argmin([np.sum(np.absolute(rectangle - char['matrix'])) for char in char_data])
+            final_output = f"{final_output}{char_data[indice]['character']}"
 
         # Unless last row, add new line
         if index != len(Z) - 1:
